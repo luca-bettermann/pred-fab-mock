795415dfe3a1d05fc3cbd11e5d47573b9963f5f0
luca_bettermann <luca.bettermann@tum.de>
2026-04-17 15:55:07 +0200

    schema adjustment

--- schema.py
+++ schema.py
@@ -31,7 +31,7 @@ def build_schema(root_folder: str = ROOT_FOLDER) -> DatasetSchema:
 
     # Spatial domain: n_layers is a design intent variable [3..8], n_segments fixed at 4
     spatial = Domain("spatial_segment", [
-        Dimension("n_layers",   "layer_idx",   min_val=5, max_val=5),
+        Dimension("n_layers",   "layer_idx",   min_val=4, max_val=8),
         Dimension("n_segments", "segment_idx", min_val=4, max_val=4),
     ])
     domains = Domains([spatial])
